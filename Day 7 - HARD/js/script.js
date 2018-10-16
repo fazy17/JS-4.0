06d072ed19678d242ca12a8eb1f81143c5998f51
FaZY <FaZY@mail.ru>
2018-10-16 21:41:18 +0300

    Day 7 HARD

--- Day 7 - HARD/js/script.js
+++ Day 7 - HARD/js/script.js
@@ -1,12 +1,24 @@
 let box = document.querySelector('.box');
 
+function animate(draw, duration) {
+    let start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        let timePassed = time - start; 
+        
+        if (timePassed > duration) timePassed = duration;
+
+        draw(timePassed);
+
+        if (timePassed < duration) {
+            requestAnimationFrame(animate);
+        }
+    });
+}
 
 box.addEventListener('click', function() {
-    let start = Date.now();
-    let timer = setInterval(function() {
-      let timePassed = Date.now() - start;
-      box.style.left = timePassed / 10 + 'px';
-      if (timePassed > 2000) clearInterval(timer);
-
-    }, 20);
+    animate(function(timePassed) {
+        box.style.left = timePassed / 1 + 'px';
+        box.style.top = timePassed / 2 + 'px';
+    }, 2000);
 });
